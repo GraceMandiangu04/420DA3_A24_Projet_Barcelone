bb9c8aadc2a064c9dd0e2bb31ac5dd4f635e1dd2
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-29 10:37:37 -0500

    PurchaseOrderService doc

--- 420DA3_A24_Projet/Business/Services/PurchaseOrderService.cs
+++ 420DA3_A24_Projet/Business/Services/PurchaseOrderService.cs
@@ -26,22 +26,50 @@ internal class PurchaseOrderService {
         this.view = new PurchaseOrderView(parentApp);
     }
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
     public PurchaseOrder? GetPOById(int id, bool includeDeleted = false) {
         return this.dao.GetById(id, includeDeleted);
     }
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="loggedInEmployeeWarehouse"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
     public List<PurchaseOrder> GetIncompletePOsForWarehouse(Entrepot loggedInEmployeeWarehouse, bool includeDeleted = false) {
         return this.dao.GetIncompletePOsForWarehouse(loggedInEmployeeWarehouse, includeDeleted);
     }
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="criterion"></param>
+    /// <param name="includeDeleted"></param>
+    /// <returns></returns>
     public List<PurchaseOrder> Search(string criterion, bool includeDeleted = false) {
         return this.dao.Search(criterion, includeDeleted);
     }
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="po"></param>
+    /// <returns></returns>
     public PurchaseOrder CreatePO(PurchaseOrder po) {
         return this.dao.Create(po);
     }
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="po"></param>
+    /// <returns></returns>
     public PurchaseOrder UpdatePO(PurchaseOrder po) {
         if (po.Status == PurchaseOrderStatusEnum.Completed) {
             po.CompletionDate = DateTime.Now;
@@ -51,6 +79,12 @@ internal class PurchaseOrderService {
         return this.dao.Update(po);
     }
 
+    /// <summary>
+    /// TODO @PROF: documenter
+    /// </summary>
+    /// <param name="po"></param>
+    /// <param name="softDeletes"></param>
+    /// <returns></returns>
     public PurchaseOrder DeletePO(PurchaseOrder po, bool softDeletes = true) {
         return this.dao.Delete(po, softDeletes);
     }
