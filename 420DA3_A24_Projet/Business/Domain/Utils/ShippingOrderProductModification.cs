6e603d61b635a0c176e4b7c827ccfbeab7c03f88
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-11-29 09:48:46 -0500

    Ajout de ShippingOrderProductModification

---
+++ 420DA3_A24_Projet/Business/Domain/Utils/ShippingOrderProductModification.cs
@@ -0,0 +1,23 @@
+﻿using _420DA3_A24_Projet.Business.Domain.Pivots;
+
+namespace _420DA3_A24_Projet.Business.Domain.Utils;
+
+internal enum ShippingOrderProductModificationTypeEnum {
+    Addition,
+    Modification,
+    Removal
+}
+
+internal class ShippingOrderProductModification {
+
+    public ShippingOrderProduct ShippingOrderProduct { get; set; }
+    public ShippingOrderProductModificationTypeEnum ModificationType { get; set; }
+    public int OriginalQuantity { get; set; }
+    public int NewQuantity { get; set; }
+
+    public ShippingOrderProductModification(ShippingOrderProduct shippingOrderProduct, ShippingOrderProductModificationTypeEnum modificationType) {
+        this.ShippingOrderProduct = shippingOrderProduct;
+        this.ModificationType = modificationType;
+    }
+
+}
