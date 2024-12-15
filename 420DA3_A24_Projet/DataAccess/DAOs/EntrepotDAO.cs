a40cfd19d399e8b50c1af979057ba88c41489871
Gracmandiangu <grace.mandiangu_ndonzuau@lcieducation.net>
2024-12-15 04:53:27 -0500

    ajout de la classe EntrepotDAO

---
+++ 420DA3_A24_Projet/DataAccess/DAOs/EntrepotDAO.cs
@@ -0,0 +1,50 @@
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_A24_Projet.DataAccess.DAOs;
+internal class EntrepotDAO {
+
+    private WsysDbContext context;
+
+    public EntrepotDAO(WsysDbContext context) {
+
+        this.context = context;
+    }
+
+    public List<Entrepot> Search(String criterion) {
+        return this.context.Entrepots
+            .Where(e => e.nomEntrepot.Contains(criterion) || e.adresse.Contains(criterion))
+            .Include(e => e.Produit)
+            .ToList();
+
+    }
+
+    public Entrepot Insert(Entrepot entrepot) {
+        _=this.context.Entrepots.Add(entrepot);
+        _ = this.context.SaveChanges();
+        return entrepot;
+    }
+
+    public Entrepot Update(Entrepot entrepot) {
+        _ = this.context.Entrepots.Update(entrepot);
+        _ = this.context.SaveChanges();
+        return entrepot;
+    }
+
+    public Entrepot Delete(Entrepot entrepot) {
+        _ = this.context.Entrepots.Remove(entrepot);
+        _ = this.context.SaveChanges();
+        return entrepot;
+    }
+
+
+
+
+
+}
