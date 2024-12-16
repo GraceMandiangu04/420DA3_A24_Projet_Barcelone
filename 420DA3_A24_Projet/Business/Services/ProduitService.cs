91d7da1961544d77e53d6468c3eb3ac14b4b975a
Gracmandiangu <grace.mandiangu_ndonzuau@lcieducation.net>
2024-12-15 22:40:41 -0500

    ajout des classes services

--- 420DA3_A24_Projet/Business/Services/ProduitService.cs
+++ 420DA3_A24_Projet/Business/Services/ProduitService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _420DA3_A24_Projet.Business.Domain;
+using _420DA3_A24_Projet.DataAccess.Contexts;
+using _420DA3_A24_Projet.DataAccess.DAOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,4 +9,25 @@ using System.Threading.Tasks;
 
 namespace _420DA3_A24_Projet.Business.Services;
 internal class ProduitService {
+    private ProduitDAO dAO;
+
+    public ProduitService(WsysApplication parentApp, WsysDbContext context) {
+        this.dAO = new ProduitDAO(context);
+    }
+
+    public List<Produit> SearchProduit(string criterion) {
+        return this.dAO.Search(criterion);
+    }
+
+    public Entrepot InsertProduit(Produit produit) {
+        return this.dAO.Insert(produit);
+    }
+
+    public Entrepot UpdateEntrepot(Produit produit) {
+        return this.dAO.Update(produit);
+    }
+
+    public Entrepot DeleteEntrepot(Entrepot entrepot) {
+        return this.dAO.Delete(entrepot);
+    }
 }
